c1b15dae577c4b7fe2fe110e72ea257ae9591803
MrFish1604 <mmrfishc@gmail.com>
2022-08-24 13:58:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup #enables develop
 
 setup(name='hotword_detection',
-      version='1.2',
+      version='1.2.1',
       description='Python Hotword Detection',
       author='Prakhar Swarup, Saketh Sharma, Hitesh Tulsiani',
-      author_email='prkhrswrp@gmail.com',
       license='IITB',
       scripts=['bin/checkHotword.py','bin/trainHotword.py'],
-      url='https://github.com/sakethgsharma/HotWordDetection',
+      url='https://github.com/MrFish1604/HotWordDetection',
       packages=['hotword_detection'],
       install_requires=[
         "numpy",
